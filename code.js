3903020dd6c49b83f9c18426f9ef721559354fd1
easrng <easrng@gmail.com>
2019-02-27 15:34:27 -0500

    commit all files

---
+++ code.js
@@ -0,0 +1,126 @@
+/*ESR SCALE MAGIC*/
+function resize(){document.body.style.setProperty('--vmin-pixels', window.innerHeight < window.innerWidth?window.innerHeight:window.innerWidth);};resize();window.onresize=resize;
+/*APPLAB CODE*/
+onEvent("button1", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("Weak", "click", function(event) {
+  setScreen("screen3");
+});
+onEvent("NextWeak1", "click", function(event) {
+  setScreen("screen4");
+});
+onEvent("HomeWeak1", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("WeakHome2", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("WeakNext2", "click", function(event) {
+  setScreen("screen5");
+});
+onEvent("WeakNext3", "click", function(event) {
+  setScreen("screen6");
+});
+onEvent("WeakHome3", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("WeakHome4", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("Boss", "click", function(event) {
+  setScreen("screen7");
+});
+onEvent("BossHome1", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("BossNext1", "click", function(event) {
+  setScreen("screen8");
+});
+onEvent("BossHome2", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("BossNext2", "click", function(event) {
+  setScreen("screen9");
+});
+onEvent("BossHome3", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("Beasts", "click", function(event) {
+  setScreen("screen10");
+});
+onEvent("BeastsNext1", "click", function(event) {
+  setScreen("screen11");
+});
+onEvent("BeastsHome1", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("BeastsNext2", "click", function(event) {
+  setScreen("screen12");
+});
+onEvent("BeastsHome2", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("BeastsNext3", "click", function(event) {
+  setScreen("screen13");
+});
+onEvent("BeastsHome3", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("BeastsHome4", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("Demons", "click", function(event) {
+  setScreen("screen14");
+});
+onEvent("DemonsHome1", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("DemonsNext1", "click", function(event) {
+  setScreen("screen15");
+});
+onEvent("DemonsHome2", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("DemonsNext2", "click", function(event) {
+  setScreen("screen16");
+});
+onEvent("DemonsHome3", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("Anamals", "click", function(event) {
+  setScreen("screen17");
+});
+onEvent("AnamalsHome1", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("AnamalsNext1", "click", function(event) {
+  setScreen("screen18");
+});
+onEvent("AnamalsNext2", "click", function(event) {
+  setScreen("screen19");
+});
+onEvent("AnamalsHome2", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("AnamalsHome3", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("Ele", "click", function(event) {
+  setScreen("screen20");
+});
+onEvent("EleNext1", "click", function(event) {
+  setScreen("screen21");
+});
+onEvent("EleHome1", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("EleNext2", "click", function(event) {
+  setScreen("screen22");
+});
+onEvent("EleHome2", "click", function(event) {
+  setScreen("screen2");
+});
+onEvent("EleHome3", "click", function(event) {
+  setScreen("screen2");
+});
